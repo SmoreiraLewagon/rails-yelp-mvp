5866a7cf7000c0bc4a1bb4430dbf817ee4cb75e2
joao sampaio <joaomarcosemgp@gmail.com>
2021-05-26 16:26:46 -0300

    seeds the database with faker

--- Gemfile
+++ Gemfile
@@ -50,3 +50,6 @@ gem 'rails-controller-testing', group: [ :test ]
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.6.1'
 gem 'simple_form'
+
+#for funny seeds
+gem 'faker', '~>2.18.0'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+10.times do
+  new_restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    phone_number: Faker::PhoneNumber.cell_phone_with_country_code
+    )
+  new_restaurant.save
+  puts "#{new_restaurant.id} - #{new_restaurant.name} was created"
+end
+
+
+puts "done."
